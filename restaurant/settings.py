5443a38c57c586e39c83c11fc24cff27d175f598
Abdul12majid <yisaabdulmajid@gmail.com>
2024-07-03 17:01:20 +0100

    changes

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -145,7 +145,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.office365.com'
+EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='yisaabdulmajid@gmail.com'
 EMAIL_HOST_PASSWORD='bhlfcekthklyqqhm'
